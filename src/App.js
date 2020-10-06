f764067e902d4a8de65a05cf23c17752f48494e5
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-10-06 14:08:46 -0400

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState((prevState) => ({clicked: !prevState.clicked}))
+  }
+
+  render() {
+    console.log ({ yes, no })
+    return (
+    <div className="app" onClick={this.clickHandler}>
+    <div className="statement">{this.state.clicked ? <h2>{yes.statement}</h2> : <h2>{no.statement}</h2>}</div>;
+    <div className="image">{this.state.clicked ? <img src={yes.image} alt=""/> : <img src={no.image} alt=""/>}</div>;
+    </div>
+    )
+  }
 }
 
 export default App;
