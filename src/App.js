17e556340c8964536d11235912440846a0468dac
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-06 14:45:53 -0400

    deliverables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { yes, no } from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+class App extends Component {
+
+  state = {
+    yesStatement: yes["yes-statement"],
+    yesImage: yes["yes-image"],
+    noStatement: no["no-statement"],
+    noImage: no["no-image"],
+  }
+
+  clickHandler = () => {
+
+  }
+
+
+  render() {
+    return( 
+      <div>
+        <h1>{this.state.yesStatement}</h1>
+        <img onClick={this.clickHandler} src={this.state.yesImage}></img>
+      </div>
+    );
+  }
 }
 
 export default App;
