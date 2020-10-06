4105c6c452bcc4cfa21e3cfa909b814a831fbbef
zhaoerik <jediofthenyy@gmail.com>
2020-10-06 14:46:03 -0400

    not done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState((prevState) => ({clicked: !prevState.clicked}))
+  }
+
+  // function to remove image and header
+
+  render () {
+    return (
+      <div>
+        <h2>{no["no-statement"]}</h2>
+        <img onClick={this.clickHandler} src={no["no-image"]}></img>
+      {this.state.clicked ?
+      // invoke remove function
+      <h2>{yes["yes-statement"]}</h2> : null 
+      }
+      {this.state.clicked ?
+      // invoke remove function
+      <img onClick={this.clickHandler} src={yes["yes-image"]}></img> : null
+      }
+      </div>
+    )
+  }
+
 }
 
 export default App;
