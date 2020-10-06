c53fea0619758929ec5170e77877cf59785fb92d
vnapuri <vladnapuri@gmail.com>
2020-10-06 14:45:10 -0400

    Final Commit No Styling yet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+class App extends Component {
+  state = {
+    responses: { yes, no },
+    clicked: true
+  }
+  
+  clickHandler = () => {
+    this.setState((prevState) => ({ clicked: !prevState.clicked }))
+  }
+  
+  
+  
+  
+  render() {
+  return (
+    <div className="card">
+    {this.state.clicked ?  <h1>{this.state.responses.yes["yes-statement"]}</h1> : <h1>{this.state.responses.no["no-statement"]}</h1>}
+    {this.state.clicked ? <img alt="" src={this.state.responses.yes['yes-image']} onClick={this.clickHandler}></img> : <img alt="" src={this.state.responses.no['no-image']} onClick={this.clickHandler}></img>}
+    </div>
+  )
+
+}
 }
 
 export default App;
