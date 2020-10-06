0d654cdbc5d5b40aee7d19e862cfbb51045d54be
anickel101 <anson.nickel@gmail.com>
2020-10-06 14:23:08 -0400

    moved logic to component below App

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import {yes, no} from './objects'
+import ImageCard from './ImageCard'
 
 class App extends Component {
 
-  state={
-    canIDoThis: false,
-    yesInfo: yes,
-    noInfo: no
-  }
-
-  getImageSrc = () => {
-    if(this.state.canIDoThis) {
-      return this.state.yesInfo["yes-image"]
-    } else {
-      return this.state.noInfo["no-image"]
-    }
-  }
-
-  getStatement = () => {
-    if(this.state.canIDoThis) {
-      return this.state.yesInfo["yes-statement"]
-    } else {
-      return this.state.noInfo["no-statement"]
-    }
-  }
-
-  changeState = () => {
-    this.setState(prevState => {
-      return {
-        canIDoThis: !prevState.canIDoThis
-      }
-    })
-  }
-
-
   render() {
     return (
-      <React.Fragment>
-        <div>
-          <h1>{this.getStatement()}</h1>
-        </div>
-        <div>
-          <img src={this.getImageSrc()} onClick={this.changeState} alt=""/>
-        </div>
-      </React.Fragment>
+      <ImageCard />
     )
   }
 }
---
+++ src/ImageCard.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {yes, no} from './objects'
+
+class ImageCard extends Component {
+
+    state={
+        canIDoThis: false,
+        yesInfo: yes,
+        noInfo: no
+    }
+
+    getImageSrc = () => {
+        if(this.state.canIDoThis) {
+        return this.state.yesInfo["yes-image"]
+        } else {
+        return this.state.noInfo["no-image"]
+        }
+    }
+
+    getStatement = () => {
+        if(this.state.canIDoThis) {
+        return this.state.yesInfo["yes-statement"]
+        } else {
+        return this.state.noInfo["no-statement"]
+        }
+    }
+
+    changeState = () => {
+        this.setState(prevState => {
+        return {
+            canIDoThis: !prevState.canIDoThis
+        }
+        })
+    }
+
+
+    render() {
+        return (
+        <React.Fragment>
+            <div>
+            <h1>{this.getStatement()}</h1>
+            </div>
+            <div>
+            <img src={this.getImageSrc()} onClick={this.changeState} alt=""/>
+            </div>
+        </React.Fragment>
+        )
+    }
+}
+
+export default ImageCard
+
+
