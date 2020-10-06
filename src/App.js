3860a55ef6380e9deee897a15b060df3bee9ed29
CanniJr <cannigialaluw@gmail.com>
2020-10-06 14:46:38 -0400

    not done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
-import {yes, no} from './objects'
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    Image: no,
+    Statement: 'no'
+  }
+
+  clickHandler = () => {
+
+  }
+
+  render() {
+    // console.log(yes, no)
+  return ( 
+   <div>
+     <h1>Statement: {this.state.Statement}</h1>
+     <img image         />
+   </div>
+    
+  );
+  }
 }
 
 export default App;
