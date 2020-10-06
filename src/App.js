517ba04d540844f7423a9067b6d2783b7fb9f365
stackguardian <mferdous98@aol.com>
2020-10-06 14:45:03 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component{
+
+  state = {
+    clicked: true
+  }
+
+  clickHandler = () => {
+
+    this.setState((prevState) =>({clicked:
+    !prevState.clicked}))
+  }
+
+render() {
+  return(
+    <>
+    <h2> {no["no-statement"]}  </h2>
+    <img src={no["no-image"]} onClick={this.clickHandler}/>
+    {this.state.clicked ? 
+    <>
+    <h2> {yes["yes-statement"]} </h2> 
+    <img src={yes["yes-image"]} /> 
+    </>
+    : null}
+    </>
+    )
+  }
 }
 
 export default App;
