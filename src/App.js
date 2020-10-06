39ffd4319b1f79b4ad39448c2728e3ace8b3534a
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-06 14:55:15 -0400

    some more

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import { yes, no } from './objects.js'
 class App extends Component {
 
   state = {
-    yesStatement: yes["yes-statement"],
-    yesImage: yes["yes-image"],
-    noStatement: no["no-statement"],
-    noImage: no["no-image"],
+      yes: {
+        yesStatement: yes["yes-statement"],
+        yesImage: yes["yes-image"]
+  
+      },
+    no: {
+      noStatement: no["no-statement"],
+      noImage: no["no-image"],
+    }
   }
 
   clickHandler = () => {
-
+    this.setState()
   }
 
 
   render() {
     return( 
       <div>
-        <h1>{this.state.yesStatement}</h1>
-        <img onClick={this.clickHandler} src={this.state.yesImage}></img>
+        <h1>{this.state.yes.yesStatement}</h1>
+        <img onClick={this.clickHandler} src={this.state.yes.yesImage}></img>
       </div>
     );
   }
