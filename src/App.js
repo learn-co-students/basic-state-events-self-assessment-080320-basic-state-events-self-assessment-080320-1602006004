d1b8e38cddfc766cc4c10f5b0c17f44c7c6136d7
Mattie43 <ericksenm93@ymail.com>
2020-10-06 14:20:28 -0400

    deliverables done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+  state={
+    yesNoObj: yes,
+    yesNoString: 'yes'
+  }
+
+  changeYes = () => {
+    this.setState((prevState) => {
+      if(this.state.yesNoString === 'yes'){
+        return {
+          yesNoObj: no,
+          yesNoString: 'no'
+        }
+      }else{
+        return {
+          yesNoObj: yes,
+          yesNoString: 'yes'
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.yesNoObj[`${this.state.yesNoString}-statement`]}</h1>
+        <img src={this.state.yesNoObj[`${this.state.yesNoString}-image`]} alt='' onClick={this.changeYes}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
