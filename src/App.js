9d808ce6ba54da6ff4052040a2723ea74143fbc1
anickel101 <anson.nickel@gmail.com>
2020-10-06 14:13:42 -0400

    Deliverables Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends Component {
+
+  state={
+    canIDoThis: false,
+    yesInfo: yes,
+    noInfo: no
+  }
+
+  getImageSrc = () => {
+    if(this.state.canIDoThis) {
+      return this.state.yesInfo["yes-image"]
+    } else {
+      return this.state.noInfo["no-image"]
+    }
+  }
+
+  getStatement = () => {
+    if(this.state.canIDoThis) {
+      return this.state.yesInfo["yes-statement"]
+    } else {
+      return this.state.noInfo["no-statement"]
+    }
+  }
+
+  changeState = () => {
+    this.setState(prevState => {
+      return {
+        canIDoThis: !prevState.canIDoThis
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <h1>{this.getStatement()}</h1>
+        </div>
+        <div>
+          <img src={this.getImageSrc()} onClick={this.changeState} alt=""/>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
