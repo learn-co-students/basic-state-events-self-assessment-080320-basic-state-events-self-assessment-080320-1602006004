0d268c0f95c896d6b39bd5b74fb9febfd2ad4b75
valnuccio <val.nuccio@gmail.com>
2020-10-06 14:30:52 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { render } from 'react-dom';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
-}
+class App extends React.Component {
 
-export default App;
+    state={
+      hasBeenClicked:false
+    }
+
+    clickChanger = () => {
+      
+        this.setState((previousState)=> {return{hasBeenClicked:!previousState.hasBeenClicked}});
+        
+
+      }
+
+    
+
+    render(){
+      return (
+        <div className='App-header' onClick={this.clickChanger}>
+            <span className ='header'>
+      {this.state.hasBeenClicked? <h2>{yes["yes-statement"]}</h2>:<h2> {no["no-statement"]}</h2>}
+          </span>
+       <span className='image'>
+         
+         {this.state.hasBeenClicked? <img src={yes["yes-image"]}/>: <img src={no["no-image"]}/>}
+       </span>
+        </div>
+      )
+    }
+    
+  }
+
+
+
+
+export default App
